ca16df442b75278070b4ec594bbad20291e353ec
kotathefriends <5070kouta@gmail.com>
2024-01-14 17:43:01 +0900

    Initial Commit

---
+++ MyCoreData/MyCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreDataApp.swift
+//  MyCoreData
+//
+//  Created by KOTA TAKAHASHI on 2024/01/14.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
